132d82bebc2268d270b9d0f41bcbe63f2c51923a
Harshit Batra <hb99960@gmail.com>
2024-08-05 12:07:06 +0530

    Component Scan XML

---
+++ src/main/java/com/componentscan/Employee.java
@@ -0,0 +1,48 @@
+package com.componentscan;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+// by default component with register bean with class name with small caps
+// employee1, employee2 won't work, unless you change the name in getBean() method
+// custom name is possible
+
+@Component("employee")
+public class Employee {
+
+    @Value("123")
+    private int employeeId;
+    @Value("Harry")
+    private String firstName;
+    @Value("${java.home}")
+    private String lastName;
+    @Value("#{2*1000}")
+    private double salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
